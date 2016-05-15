3102c8865f75bbf97c5be475e965a7e794942c72
Subhajit Ray <subhajit.ray69@gmail.com>
2016-05-15 18:46:26 +0530

    Add files via upload

---
+++ ClientApp/Constant/Constant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test
+{
+    public class Constant
+    {
+        public static string MessageLoggingApiUrl = "MessageLoggingApiUrl";
+        public static string ExceptionLoggingApiUrl = "ExceptionLoggingApiUrl";
+        public static string LoggingType = "LoggingType";
+        public static string loggingTypeValue = "loggingTypeValue";
+        public static string CSV = ".csv";
+        public static string DB = "DB";
+        public static string File = "File";
+        public static string All = "All";
+        public static string Controller = "controller";
+        public static string Action = "action";
+        public static string ErrorViewName = "Error";
+        public static string REQUEST_METHOD = "REQUEST_METHOD";
+        public static string LOCAL_ADDR = "LOCAL_ADDR";
+        public static string QUERY_STRING = "QUERY_STRING";
+        public static string CONTENT_TYPE = "CONTENT_TYPE";
+        public static string CONTENT_LENGTH = "CONTENT_LENGTH";
+        public static string AUTH_USER = "AUTH_USER";
+        public static string REMOTE_USER = "REMOTE_USER";
+        public static string Colon = ":";
+        public static string SemiColon = ";";
+    }
+}
